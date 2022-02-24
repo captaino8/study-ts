547e11de7976c2c82aac47d29437df57373dbeef
liutian <809143962@qq.com>
2022-02-24 21:57:40 +0800

    add array

--- test-dts/baseType/baseType.d-test.ts
+++ test-dts/baseType/baseType.d-test.ts
@@ -15,4 +15,21 @@ describe("基础类型", () => {
     const str = "123";
     expectType<string>(str);
   });
+  describe("array", () => {
+    describe("类型 + 方括号 表示法", () => {
+      let arr: number[] = [1, 2, 3]
+    })
+    describe("错误示例", () => {
+      describe("不能出现其他类型", () => {
+        let items: string[] = [1, "b", "c"];
+      })
+      describe("不能 push 其它类型", () => {
+        let items: string[] = ['ws', 'wd']
+        items.push(1)
+      })
+    })
+    describe("泛型表示法", () => {
+      let arr: Array<number> = [1, 2, 3]
+    })
+  })
 })
